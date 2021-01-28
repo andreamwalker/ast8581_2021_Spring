111bacb5f45facfeb92c83339c45395dad828a9d
walk0798 <walk0798@umn.edu>
2021-01-28 12:47:43 -0600

    diff script update

--- diff_cmds.sh
+++ diff_cmds.sh
@@ -1,3 +1,10 @@
+echo "git diff --name-status inclass master"
 git diff --name-status inclass master
-
+echo "git diff --name-status master inclass"
 git diff --name-status  master inclass
+echo "To add file from master to inclass, 
+      $ git branch inclass #switch branches 
+      $ git checkout master <filename> #adds file from master 
+      repeat for all files then commit and push.
+      Note that this overwrites files w/ same name."
+
